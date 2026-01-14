c08afc35abf97e72bd799fcddc4810eb6a0c9b65
Vedant <48868398+vedantb2@users.noreply.github.com>
2026-01-14 09:56:32 +0000

    fix auth issue

--- web/lib/components/ClientProvider.tsx
+++ web/lib/components/ClientProvider.tsx
@@ -7,8 +7,8 @@ import { useAuth } from "@clerk/nextjs";
 import { ConvexQueryCacheProvider } from "convex-helpers/react/cache/provider";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProvider } from "../contexts/ThemeContext";
-import { EnsureUser } from "./EnsureUser";
 import { clientEnv } from "@/env/client";
+import { useRouter } from "next/navigation";
 
 if (!clientEnv.NEXT_PUBLIC_CONVEX_URL) {
   throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in your .env file");
@@ -17,24 +17,28 @@ if (!clientEnv.NEXT_PUBLIC_CONVEX_URL) {
 const convex = new ConvexReactClient(clientEnv.NEXT_PUBLIC_CONVEX_URL);
 
 export function ClientProvider({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       <ConvexQueryCacheProvider>
-        <EnsureUser>
-          <NextThemesProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <ThemeProvider>
-              <HeroUIProvider>
-                <ToastProvider placement="top-center" />
-                {children}
-              </HeroUIProvider>
-            </ThemeProvider>
-          </NextThemesProvider>
-        </EnsureUser>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ThemeProvider>
+            <HeroUIProvider
+              // disableAnimation={true}
+              // skipFramerMotionAnimations={false}
+              navigate={router.push}
+            >
+              <ToastProvider placement="top-center" />
+              {children}
+            </HeroUIProvider>
+          </ThemeProvider>
+        </NextThemesProvider>
       </ConvexQueryCacheProvider>
     </ConvexProviderWithClerk>
   );

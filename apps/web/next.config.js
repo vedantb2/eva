7e4ee3bb7ace41f520288eb393c57ea953007817
Eva <48868398+vedantb2@users.noreply.github.com>
2026-03-04 12:14:10 +0000

    feat: fix sessions id issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -17,6 +17,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

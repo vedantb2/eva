7a587d9f881500242480edfa4e9f59e8de52028c
Vedant <48868398+vedantb2@users.noreply.github.com>
2026-03-08 23:57:44 +0000

    Revert "fix: make page header bg extend full width on home and global pages"
    This reverts commit 1e801d351ecee0701667db0f9567277979e1d754.

--- apps/web/app/(global)/layout.tsx
+++ apps/web/app/(global)/layout.tsx
@@ -17,7 +17,9 @@ export default function GlobalLayout({
           className="pointer-events-none fixed inset-x-0 top-0 z-0 h-56 bg-gradient-to-b from-background/90 via-background/45 to-transparent"
         />
         <TopNavBar />
-        <div className="relative z-10">{children}</div>
+        <div className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          {children}
+        </div>
         <NotificationToastStream />
       </div>
     </ClientProvider>
